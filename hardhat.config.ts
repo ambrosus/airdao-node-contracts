0dd668c33f896b933b5dc6e7284f28708b10a496
svin <svinerus@gmail.com>
2023-02-28 18:14:19 +0200

    Add deployments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import {ethers} from "ethers";
+import * as dotenv from "dotenv";
+
+
+dotenv.config();
+
 
 const config: HardhatUserConfig = {
 
@@ -11,6 +17,16 @@ const config: HardhatUserConfig = {
         "0x5b18f0adcca221f65373b20158f95313ecd51bde42b96a4c16f5eb851576bc06"
       ],
       hardfork: "byzantium",
+    },
+    "test/amb": {
+      url: "https://network.ambrosus-test.io",
+      hardfork: "byzantium",
+      accounts: [process.env.PRIVATEKEY_OWNER_AMB || ethers.constants.HashZero],
+    },
+    "main/amb": {
+      url: "https://network.ambrosus.io",
+      hardfork: "byzantium",
+      accounts: [process.env.PRIVATEKEY_OWNER_AMB || ethers.constants.HashZero],
     },
   },
 
---
+++ src/utils/deployments.ts
@@ -0,0 +1,57 @@
+import path from "path";
+import fs from "fs";
+import {BaseContract, ethers, Signer} from "ethers";
+import deploymentsProd from "../../deployments/prod.json";
+import deploymentsTest from "../../deployments/test.json";
+
+export interface Deployment {
+  address: string,
+  abi: any[],
+  deployTx: string
+}
+
+export async function deploy(contractName: string, networkName: string, deployPromise: Promise<any>): Promise<any> {
+  const {path, deployments} = _loadDeployments(networkName);
+  console.assert(deployments[contractName] === undefined, "Already deployed")
+
+  console.log(`deploying ${contractName} in ${networkName}...`)
+  const res = await deployPromise;
+  await res.deployed();
+  console.log(`deployed ${contractName} at`, res.address)
+
+  deployments[contractName] = {
+    address: res.address,
+    abi: res.interface.format() as string[],
+    deployTx: res.deployTransaction.hash,
+  };
+  fs.writeFileSync(path, JSON.stringify(deployments, null, 2));
+
+  return res
+}
+
+export async function loadDeployment(contractName: string, networkName: string, signer?: Signer) {
+  const {deployments} = _loadDeployments(networkName);
+  const contractDeployment = deployments[contractName];
+  console.assert(contractDeployment !== undefined, `Can't find deployment for ${contractName} in ${networkName}`)
+
+  return new ethers.Contract(contractDeployment.address, contractDeployment.abi, signer)
+}
+
+export function loadAllDeployments(networkName: string, signer?: Signer): { [name: string]: BaseContract } {
+  const {deployments} = _loadDeployments(networkName);
+  const result: { [name: string]: BaseContract } = {};
+  for (let name of Object.keys(deployments)) {
+    const contractDeployment = deployments[name];
+    result[name] = new ethers.Contract(contractDeployment.address, contractDeployment.abi, signer)
+  }
+  return result;
+}
+
+
+// todo i don't like it
+function _loadDeployments(networkName: string): { path: string, deployments: { [name: string]: Deployment } } {
+  const deploymentPath = (name:string) => path.resolve(__dirname, `../../deployments/${name}.json`);
+  if (networkName == "22040") return {path: deploymentPath("test"), deployments: deploymentsTest};
+  if (networkName == "16718") return {path: deploymentPath("prod"), deployments: deploymentsProd};
+  throw new Error("unknown chainid");
+}
