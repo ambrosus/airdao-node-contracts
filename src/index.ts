449cbab059987a732f4755d6d656512e13602fbf
igorkishko <kishko.igor@gmail.com>
2023-03-01 14:56:29 +0200

    add new export structure

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as Confirmations from './multisig/confirmations';
+import * as Methods from './multisig/methods';
+import * as Permissions from './multisig/permissions';
+
+import { Contracts } from './contracts/contracts';
+import { ContractNames } from './contracts/names';
+
+export { Confirmations, Methods, Permissions, Contracts, ContractNames };
