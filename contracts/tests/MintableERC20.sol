c6c3a87416a8504bec468a69215e7df3e3ada297
svin <svinerus@gmail.com>
2023-04-04 03:38:40 +0300

    Use `MintableERC20` test contract in `LockKeeper` tests instead of `AmbBond`

---
+++ contracts/tests/MintableERC20.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MintableERC20 is ERC20 {
+
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+
+}
