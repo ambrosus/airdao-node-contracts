2ca91bf70233960e389fcacc9af02a9dcfd18e24
svin <svinerus@gmail.com>
2023-04-16 02:44:39 +0300

    Deploy AirDrop

--- scripts/deploy_airdrop.ts
+++ scripts/deploy_airdrop.ts
@@ -12,30 +12,15 @@ async function main() {
 
   if (networkName == "main") {
     console.log("--- MAINNET DEPLOYMENT ---");
+    console.log("Deployer: ", deployer.address);
 
-    const BACKEND_ADDRESS = ""; // todo
+    const BACKEND_ADDRESS = "0x473C539f6a042AEdfECa6A756d15Db2540202270";
 
-    const financeInvestorsMultisig = loadDeployment(ContractNames.FinanceInvestorsMultisig, networkName).address;
+    // const financeInvestorsMultisig = loadDeployment(ContractNames.FinanceInvestorsMultisig, networkName).address;
     const airBond = await deploy<AirBond__factory>(
       ContractNames.AirBond,
       networkName,
-      "AmbBond",
-      [financeInvestorsMultisig],
-      deployer,
-      true
-    );
-    await deploy<AirDrop__factory>(
-      ContractNames.AirDrop,
-      networkName,
-      "AirDrop",
-      [airBond.address, BACKEND_ADDRESS, ethers.utils.parseEther("1000")],
-      deployer
-    );
-  } else {
-    const airBond = await deploy<AirBond__factory>(
-      ContractNames.AirBond,
-      networkName,
-      "AmbBond",
+      "AirBond",
       [deployer.address],
       deployer,
       true
@@ -44,10 +29,32 @@ async function main() {
       ContractNames.AirDrop,
       networkName,
       "AirDrop",
-      [airBond.address, "0x6cde5C2473DAcc1b80142D3d54ae65Cf97355682", ethers.utils.parseEther("1000")],
+      [airBond.address, BACKEND_ADDRESS, ethers.utils.parseEther("999")],
+      deployer,
+      true
+    );
+
+    await airBond.grantRole(await airBond.DEFAULT_ADMIN_ROLE(), "0x7fadc4729bc1Ff8DAc313cb41e7527Dc6cAB2deb"); // valar
+    await airBond.grantRole(await airBond.MINTER_ROLE(), deployer.address);
+    await airBond.mint(airDrop.address, ethers.utils.parseEther("5000000000"));
+    await airDrop.transferOwnership("0x7fadc4729bc1Ff8DAc313cb41e7527Dc6cAB2deb"); // valar
+  } else {
+    const airBond = await deploy<AirBond__factory>(
+      ContractNames.AirBond,
+      networkName,
+      "AirBond",
+      [deployer.address],
+      deployer,
+      true
+    );
+    const airDrop = await deploy<AirDrop__factory>(
+      ContractNames.AirDrop,
+      networkName,
+      "AirDrop",
+      [airBond.address, "0x6cde5C2473DAcc1b80142D3d54ae65Cf97355682", ethers.utils.parseEther("999")],
       deployer
     );
-    await airBond.mint(airDrop.address, ethers.utils.parseEther("10000000000"));
+    await airBond.mint(airDrop.address, ethers.utils.parseEther("5000000000"));
   }
 }
 
