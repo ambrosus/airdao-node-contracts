b7d40cdd2092c70187f528951f34488ab7f49be8
svin <svinerus@gmail.com>
2023-07-03 11:10:39 +0300

    Update tests for BaseNodes_Manager

--- test/staking/BaseNodes.ts
+++ test/staking/BaseNodes.ts
@@ -2,6 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { ethers, upgrades } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { BaseNodes_Manager, TEST_ValidatorSet } from "../../typechain-types";
+import { expect } from "chai";
 
 describe("BaseNodes", function () {
   let validatorSet: TEST_ValidatorSet;
@@ -31,17 +32,68 @@ describe("BaseNodes", function () {
     ({ validatorSet, baseNodes, owner } = await loadFixture(deploy));
   });
 
-  describe("reward", function () {
-    it("reward", async function () {
-      await baseNodes.addStake(owner.address, { value: 50 });
-      await asSuperUser(validatorSet.reward([owner.address], [1]));
+  describe("addStake", function () {
+    it("ok", async function () {
+      await expect(baseNodes.addStake(owner.address, { value: 50 })).to.changeEtherBalance(owner, -50);
+      expect(await validatorSet.getNodeStake(owner.address)).to.be.equal(50);
+    });
+
+    it("not from admin", async function () {
+      const [_, notAdmin] = await ethers.getSigners();
+      await expect(baseNodes.connect(notAdmin).addStake(owner.address, { value: 50 })).to.be.revertedWith(
+        `AccessControl: account ${notAdmin.address.toLowerCase()} is missing role 0x0000000000000000000000000000000000000000000000000000000000000000`
+      );
     });
   });
 
-  async function asSuperUser(call: Promise<any>) {
-    await ethers.provider.send("hardhat_setCoinbase", ["0x0000000000000000000000000000000000000000"]); // superuser flag
-    const res = await call;
-    await ethers.provider.send("hardhat_setCoinbase", ["0x1111111111111111111111111111111111111111"]); // disable superuser flag
-    return res;
-  }
+  describe("removeStake", function () {
+    beforeEach(async function () {
+      await baseNodes.addStake(owner.address, { value: 50 });
+    });
+
+    it("remove all stake", async function () {
+      await expect(baseNodes.removeStake(owner.address, 50)).to.changeEtherBalance(owner, 50);
+      expect(await validatorSet.getNodeStake(owner.address)).to.be.equal(0);
+    });
+    it("remove part of stake", async function () {
+      await expect(baseNodes.removeStake(owner.address, 30)).to.changeEtherBalance(owner, 30);
+      expect(await validatorSet.getNodeStake(owner.address)).to.be.equal(20);
+    });
+    it("remove more than staked", async function () {
+      await expect(baseNodes.removeStake(owner.address, 100)).to.be.revertedWith("Stake < amount");
+    });
+
+    it("not from admin", async function () {
+      const [_, notAdmin] = await ethers.getSigners();
+      await expect(baseNodes.connect(notAdmin).removeStake(owner.address, 50)).to.be.revertedWith(
+        `AccessControl: account ${notAdmin.address.toLowerCase()} is missing role 0x0000000000000000000000000000000000000000000000000000000000000000`
+      );
+    });
+  });
+
+  describe("reward", function () {
+    beforeEach(async function () {
+      await baseNodes.addStake(owner.address, { value: 50 });
+    });
+
+    it("ok", async function () {
+      await asSuperUser(validatorSet.reward([owner.address], [1]));
+    });
+
+    it("not from validatorSet", async function () {
+      await expect(baseNodes.reward(owner.address, 50)).to.be.revertedWith("Only validatorSet can call reward()");
+    });
+  });
+
+  it("report", async function () {
+    // do nothing, for coverage
+    await baseNodes.report(owner.address);
+  });
 });
+
+async function asSuperUser(call: Promise<any>) {
+  await ethers.provider.send("hardhat_setCoinbase", ["0x0000000000000000000000000000000000000000"]); // superuser flag
+  const res = await call;
+  await ethers.provider.send("hardhat_setCoinbase", ["0x1111111111111111111111111111111111111111"]); // disable superuser flag
+  return res;
+}
