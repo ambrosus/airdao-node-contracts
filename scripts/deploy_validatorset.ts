56e2273d7028fe66a8d0235d77fff4b3821b8803
svin <svinerus@gmail.com>
2023-07-03 10:47:16 +0300

    Add multisig for validator set in deploy script

--- scripts/deploy_validatorset.ts
+++ scripts/deploy_validatorset.ts
@@ -1,14 +1,23 @@
 import { ethers } from "hardhat";
 import { ContractNames } from "../src";
 import { deploy } from "../src/dev/deploy";
-import { ValidatorSet__factory } from "../typechain-types";
-import { chainIDToName } from "../src/utils/deployments";
+import { Multisig__factory, ValidatorSet__factory } from "../typechain-types";
+import { chainIDToName, loadDeployment } from "../src/utils/deployments";
 
 async function main() {
   const chainId = (await ethers.provider.getNetwork()).chainId;
   const networkName = chainIDToName[chainId];
 
   const [deployer] = await ethers.getSigners();
+  const masterMultisig = loadDeployment(ContractNames.MasterMultisig, networkName).address;
+
+  const multisig = await deploy<Multisig__factory>(
+    ContractNames.ValidatorSetMultisig,
+    networkName,
+    "Multisig",
+    [[deployer.address], [true], 75, masterMultisig],
+    deployer
+  );
 
   // todo
   const validatorSet = await deploy<ValidatorSet__factory>(
