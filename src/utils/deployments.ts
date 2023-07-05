b0a69c2e4084013600e61e0dced92d6dc94d53bd
serezhaolshan <olshan228@gmail.com>
2023-07-05 16:53:23 +0300

    feat: add dynamic imports

--- src/utils/deployments.ts
+++ src/utils/deployments.ts
@@ -1,8 +1,4 @@
 import { Contract, ethers, Signer } from "ethers";
-import deploymentsMain from "../../deployments/16718.json";
-import deploymentsTest from "../../deployments/22040.json";
-import deploymentsDev from "../../deployments/30746.json";
-import deploymentsLocal from "../../deployments/5256.json";
 
 export interface Deployment {
   address: string;
@@ -22,7 +18,7 @@ export function loadDeployment(contractName: string, networkId: number, signer?:
   return _contractFromDeployment(deployments[contractName], signer);
 }
 
-export function loadAllDeployments(networkId: number, signer?: Signer): { [name: string]: Contract } {
+export function loadAllDeployments(networkId: number, signer?: Signer): { [ name: string ]: Contract } {
   const deployments = _loadDeployments(networkId);
   const result: { [name: string]: Contract } = {};
 
@@ -35,11 +31,6 @@ export function _contractFromDeployment(deployment: Deployment, signer?: Signer)
   return new ethers.Contract(deployment.address, deployment.abi, signer);
 }
 
-// todo i don't like it
-export function _loadDeployments(networkId: number): { [name: string]: Deployment } {
-  if (networkId == 30746) return deploymentsDev;
-  if (networkId == 22040) return deploymentsTest;
-  if (networkId == 16718) return deploymentsMain;
-  if (networkId == 0x1488) return deploymentsLocal;
-  throw new Error(`unknown network id: ${networkId}`);
+export function _loadDeployments(chainId: number): { [ name: string ]: Deployment } {
+  return require(`../../deployments/${chainId}.json`);
 }
