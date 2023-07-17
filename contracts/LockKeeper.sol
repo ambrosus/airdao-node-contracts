4c57a4b7847db22fc0660b2b233796847508d9fe
svin <svinerus@gmail.com>
2023-07-17 21:04:27 +0300

    Use SafeERC20

--- contracts/LockKeeper.sol
+++ contracts/LockKeeper.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.0;
 
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 
 contract LockKeeper {
+    using SafeERC20 for IERC20;
+
     constructor() {
 
     }
