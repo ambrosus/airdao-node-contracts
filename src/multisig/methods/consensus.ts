744b33e363f83234f984f672a3821254ff092aa4
serezhaolshan <olshan228@gmail.com>
2023-08-07 16:36:13 +0300

    feat: add exports to consensus multisig methods

--- src/multisig/methods/consensus.ts
+++ src/multisig/methods/consensus.ts
@@ -4,45 +4,45 @@ import { ContractNames } from "../../contracts/names";
 import { submitTransaction2 } from "./internal";
 
 // view methods
-async function validatorSetGetNodeStake(contracts: Contracts, nodeAddress: string) {
+export async function validatorSetGetNodeStake(contracts: Contracts, nodeAddress: string) {
   const validatorSet = contracts.getContractByName(ContractNames.ValidatorSet) as ValidatorSet;
   return validatorSet.getNodeStake(nodeAddress);
 }
 
-async function validatorSetGetValidators(contracts: Contracts) {
+export async function validatorSetGetValidators(contracts: Contracts) {
   const validatorSet = contracts.getContractByName(ContractNames.ValidatorSet) as ValidatorSet;
   return validatorSet.getValidators();
 }
 
-async function validatorSetGetTopStakes(contracts: Contracts) {
+export async function validatorSetGetTopStakes(contracts: Contracts) {
   const validatorSet = contracts.getContractByName(ContractNames.ValidatorSet) as ValidatorSet;
   return validatorSet.getTopStakes();
 }
 
-async function validatorSetGetQueuedStakes(contracts: Contracts) {
+export async function validatorSetGetQueuedStakes(contracts: Contracts) {
   const validatorSet = contracts.getContractByName(ContractNames.ValidatorSet) as ValidatorSet;
   return validatorSet.getQueuedStakes();
 }
 
-async function validatorSetGetStakesByManager(contracts: Contracts, managerAddress: string) {
+export async function validatorSetGetStakesByManager(contracts: Contracts, managerAddress: string) {
   const validatorSet = contracts.getContractByName(ContractNames.ValidatorSet) as ValidatorSet;
   return validatorSet.getStakesByManager(managerAddress);
 }
 
-async function validatorSetGetBlockListeners(contracts: Contracts) {
+export async function validatorSetGetBlockListeners(contracts: Contracts) {
   const validatorSet = contracts.getContractByName(ContractNames.ValidatorSet) as ValidatorSet;
   return validatorSet.listeners();
 }
 
 // admin methods
 
-async function validatorSetAddBlockListener(contracts: Contracts, listener: string) {
+export async function validatorSetAddBlockListener(contracts: Contracts, listener: string) {
   return await submitTransaction2<ValidatorSet>(contracts, ContractNames.ValidatorSet, 0, (validatorSet) =>
     validatorSet.addBlockListener(listener)
   );
 }
 
-async function validatorSetRemoveBlockListener(contracts: Contracts, listener: string) {
+export async function validatorSetRemoveBlockListener(contracts: Contracts, listener: string) {
   return await submitTransaction2<ValidatorSet>(contracts, ContractNames.ValidatorSet, 0, (validatorSet) =>
     validatorSet.removeBlockListener(listener)
   );
