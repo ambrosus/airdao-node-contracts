0b71dbbf5d06637ac672e6800df282b89c5d6779
serezhaolshan <olshan228@gmail.com>
2023-08-07 12:29:24 +0300

    feat: error provider in lib

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import Multisig from "./multisig";
 
 import { Contracts } from "./contracts/contracts";
 import { ContractNames } from "./contracts/names";
-import { AmbErrorProviderWeb3, AmbErrorProvider } from "./utils/AmbErrorProvider";
+import { AmbErrorProviderWeb3, AmbErrorProvider } from "@airdao/error-provider";
 
 export { Multisig, Contracts, ContractNames, AmbErrorProviderWeb3, AmbErrorProvider };
