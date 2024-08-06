1683eca6e46a94f5b0b8b700659cc4eec84fafe3
svin <svinerus@gmail.com>
2024-08-06 17:33:49 +0300

    Deploy astratokensafe to dev/test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,7 +28,7 @@ const config: HardhatUserConfig = {
     dev: {
       url: "https://network.ambrosus-dev.io",
       hardfork: "byzantium",
-      gasPrice: 0,
+      // gasPrice: 0,
       accounts: [
         process.env.PRIVATEKEY_OWNER_AMB! || ethers.constants.HashZero,
         process.env.PRIVATEKEY_TEST_MULTISIG1! || ethers.constants.HashZero,
@@ -38,7 +38,7 @@ const config: HardhatUserConfig = {
     test: {
       url: "https://network.ambrosus-test.io",
       hardfork: "byzantium",
-      gasPrice: 0,
+      // gasPrice: 0,
       accounts: [
         process.env.PRIVATEKEY_OWNER_AMB || ethers.constants.HashZero,
         process.env.PRIVATEKEY_TEST_MULTISIG1 || ethers.constants.HashZero,
