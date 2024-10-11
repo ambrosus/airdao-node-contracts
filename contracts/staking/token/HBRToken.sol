478a0370d2d21be38a3b4172257abcd622f4340a
SigismundSchlomo <o.kuznietsov@inc4.net>
2024-10-11 14:49:44 +0300

    Update HBR token

--- contracts/staking/token/HBRToken.sol
+++ contracts/staking/token/HBRToken.sol
@@ -1,21 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
-contract HBRToken is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");  // can use mint / burn methods
+contract HBRToken is ERC20Pausable, Ownable {
 
-    constructor(address admin) ERC20("Harbor", "HBR") {
-        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+    constructor(address admin) ERC20("Harbor", "HBR") Ownable() {
+        _transferOwnership(admin);
     }
 
-    function mint(address account, uint256 amount) external onlyRole(MINTER_ROLE) {
+    function mint(address account, uint256 amount) external onlyOwner {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) external onlyRole(MINTER_ROLE) {
+    function burn(address account, uint256 amount) external onlyOwner {
         _burn(account, amount);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
