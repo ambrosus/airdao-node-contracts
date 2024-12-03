fab420a4401b78debb83d815907af4c033c4626a
SigismundSchlomo <o.kuznietsov@inc4.net>
2024-12-03 21:27:00 +0200

    WIP

--- scripts/utils/deployMultisig.ts
+++ scripts/utils/deployMultisig.ts
@@ -1,33 +1,36 @@
-import { Multisig__factory } from "../../typechain-types";
 import { ContractNames } from "../../src";
 import { EcosystemMultisigSettings, Roadmap2023MultisigSettings } from "./addresses";
 import { Signer } from "ethers";
-import { loadDeployment, deploy } from "@airdao/deployments/deploying";
-
+import { loadDeployment, getMultisigFactory, MultisigSettings } from "@airdao/deployments/deploying";
 
 export async function deployMultisig(contractName: string, signer: Signer, version: "common" | "eco" = "common") {
 
   const chainId = (await signer.provider!.getNetwork()).chainId;
 
-
-  let deployArgs: any = [];
+  const settings: MultisigSettings = {
+    owner: "",
+    signers: [],
+    isInitiatorFlags: [],
+    threshold: 0,
+  };
 
   if (version === "eco") {
     const ecosystemMasterMultisig = loadDeployment(ContractNames.Ecosystem_MasterMultisig, chainId).address;
-    deployArgs = [...EcosystemMultisigSettings, ecosystemMasterMultisig];
+    settings.owner = ecosystemMasterMultisig;
+    settings.signers = EcosystemMultisigSettings[0];
+    settings.isInitiatorFlags = EcosystemMultisigSettings[1];
+    settings.threshold = EcosystemMultisigSettings[2];
   } else {
     const masterMultisig = loadDeployment(ContractNames.MasterMultisig, chainId).address;
-    deployArgs = [...Roadmap2023MultisigSettings, masterMultisig];
+    settings.owner = masterMultisig;
+    settings.signers = Roadmap2023MultisigSettings[0];
+    settings.isInitiatorFlags = Roadmap2023MultisigSettings[1];
+    settings.threshold = Roadmap2023MultisigSettings[2];
   }
 
+  const factory = getMultisigFactory(chainId);
+
   const isMainnet = chainId === 16718;
-  if (!isMainnet) deployArgs[2] = 1;  // threshold
-
-
-  return await deploy<Multisig__factory>({
-    artifactName: "Multisig",
-    contractName, deployArgs, signer,
-    loadIfAlreadyDeployed: true,
-  });
+  if (!isMainnet) settings.threshold = 1;// threshold
 
 }
