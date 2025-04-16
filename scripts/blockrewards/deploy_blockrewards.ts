0279ca574dcdd3fa5caa5529818e999f88882c2c
svin <svinerus@gmail.com>
2025-04-16 13:48:42 +0300

    call validatorset.setBlockRewardsContract after deploying blockrewards contract; include deploy_blockrewards script to redeploy.sh

--- scripts/blockrewards/deploy_blockrewards.ts
+++ scripts/blockrewards/deploy_blockrewards.ts
@@ -1,11 +1,12 @@
-import {deploy} from "@airdao/deployments/deploying";
-import {ethers} from "hardhat";
-import {BlockRewardsMode2__factory, BlockRewardsMode2, Multisig__factory} from "../../typechain-types";
+import { deploy, loadDeployment } from "@airdao/deployments/deploying";
+import { ethers, network } from "hardhat";
+import { BlockRewardsMode2__factory, BlockRewardsMode2, Multisig__factory, ValidatorSet } from "../../typechain-types";
 import { ContractNames } from "../../src";
 import { deployMultisig } from "../utils/deployMultisig";
 
 export async function main() {
   const [deployer] = await ethers.getSigners();
+  const { chainId } = await ethers.provider.getNetwork();
 
   const multisig = await deployMultisig(ContractNames.BlockRewardsMode2_Multisig, deployer);
 
@@ -16,6 +17,13 @@ export async function main() {
     signer: deployer,
     isUpgradeableProxy: false,
   });
+
+  if (network.name != "main") {
+    const validatorSet = loadDeployment(ContractNames.ValidatorSet , chainId, deployer) as ValidatorSet;
+    await (await validatorSet.setBlockRewardsContract(blockRewards.address)).wait();
+  }
+
+
 }
 
 if (require.main === module) {
--- scripts/redeploy/redeploy.sh
+++ scripts/redeploy/redeploy.sh
@@ -25,6 +25,8 @@ npx hardhat run scripts/staking/deploy_servernodes_manager.ts --network "${NETWO
 npx hardhat run scripts/staking/deploy_legacy_pool_manager.ts --network "${NETWORK}"
 npx hardhat run scripts/staking/migrate_to_new_staking.ts --network "${NETWORK}"
 npx hardhat run scripts/fees/deploy_fees.ts --network "${NETWORK}"
+npx hardhat run scripts/blockrewards/deploy_blockrewards.ts --network "${NETWORK}"
+
 
 npx hardhat run scripts/multisig/deploy_ecosystem_multisig.ts --network "${NETWORK}"
 npx hardhat run scripts/ecosystem/bond-marketplace/deploy.ts --network "${NETWORK}"
